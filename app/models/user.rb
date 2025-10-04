b906dfe3594ff58216b4c4403b507eb76f4de1f8
AyaneRan <domufuruya26@gmail.com>
2025-10-05 00:29:30 +0900

    ７章までの自力制作

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_secure_password
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :email, presence: true, length: { maximum: 255 },
+                      format: { with: URI::MailTo::EMAIL_REGEXP },
+                      uniqueness: { case_sensitive: false }
+    validates :password, presence: true, length: { minimum: 6 } 
+end
