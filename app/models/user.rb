1902f4e215ee6f1e679c230f29cb2e2460b0f534
AyaneRan <domufuruya26@gmail.com>
2025-10-08 13:56:18 +0900

    created the login form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,26 @@ class User < ApplicationRecord
                       format: { with: URI::MailTo::EMAIL_REGEXP },
                       uniqueness: { case_sensitive: false }
     validates :password, presence: true, length: { minimum: 6 } 
+
+  attr_accessor :remember_token
+
+
+  def remember
+    self.remember_token = Secure.random.urlsafe_base64
+    update!(remember_digest: BCrypt::Password.create(remember_token))
+  end 
+
+  def authenticated?(token)
+     BCrypt::Password.new(remember_digest).is_password?(token)
+  end
+
+  def forget
+    update!(remember_digest: nil)
+  end
+  has_secure_password
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :email, presence: true, length: { maximum: 255 },
+                      format: { with: URI::MailTo::EMAIL_REGEXP },
+                      uniqueness: { case_sensitive: false }
+    validates :password, presence: true, length: { minimum: 6 } 
 end
